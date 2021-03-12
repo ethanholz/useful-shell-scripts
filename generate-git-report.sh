634781231ef76a10868313f9e1e50bac1f5392f5
Ethan Holz <ethan2@vt.edu>
2021-03-12 10:38:14 -0500

    Added new functionality to specifiy arbitary repos

--- generate-git-report.sh
+++ generate-git-report.sh
@@ -1,17 +1,39 @@
 #!/usr/bin/env bash
+function write_repo_history {
+	git -C $1 fetch
+	string=$(git -C $1 log --pretty="format:%<(20)%ae %<(20)%ai %<(20) %s" --all --since="last week")
+	len=`expr length "$string"`
+	if [ ! $len -eq 0 ]; then
+		echo -e "REPO: $1\n$string\n" >> git-log-`date -I`.txt
+	fi
+	
+}
+
+function check_repo {
+	if [ -d "$1" ]; then
+		if git -C $1 rev-parse --git-dir > /dev/null 2>&1; then
+			echo "This is a git repo: $1"
+			write_repo_history $1
+		fi
+	fi
+}
+
+
 if [ -f "git-log-`date -I`.txt" ]; then
 	rm git-log-`date -I`.txt
 fi
-for f in *; do
-	if [ -d "$f" ]; then
-		if git -C $f rev-parse --git-dir > /dev/null 2>&1; then
-			echo "This is a git repo: $f"
-			git -C $f fetch
-			string=$(git -C $f log --pretty="format:%<(20)%ae %<(20)%ai %<(20) %s" --all --since="last week")
-			len=`expr length "$string"`
-			if [ ! $len -eq 0 ]; then
-				echo -e "REPO: $f\n$string\n" >> git-log-`date -I`.txt
-			fi
-		fi
-	fi
+while getopts "r:" opt; do
+	case $opt in
+		r) repos+=("$OPTARG");;
+	esac
 done
+if [ ${#repos[@]} -eq 0 ]; then
+	for f in *; do
+		check_repo $f
+	done
+else
+	for repo in "${repos[@]}"; do
+		check_repo $repo
+	done
+fi
+
