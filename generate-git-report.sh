33f44eb002787753b98d8b1bafe092225f9805a4
Ethan Holz <ethan.holz@vt.edu>
2021-03-18 21:05:35 -0400

    Updated naming to get remote name

--- generate-git-report.sh
+++ generate-git-report.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 function write_repo_history {
 	git -C $1 fetch
+	repo_name=$(basename $(git -C $1 remote get-url origin) .git)
 	string=$(git -C $1 log --pretty="format:%<(20)%ae %<(20)%ai %<(20) %s" --all --since="last week")
 	len=`expr length "$string"`
 	if [ ! $len -eq 0 ]; then
-		echo -e "REPO: $1\n$string\n" >> git-log-`date -I`.txt
-	fi
+		echo -e "REPO: $repo_name\n$string\n" >> git-log-`date -I`.txt
+	fi	
 	
 }
 
@@ -15,9 +16,10 @@ function check_repo {
 			echo "This is a git repo: $1"
 			write_repo_history $1
 		fi
+		
 	fi
 }
-
+	
 
 if [ -f "git-log-`date -I`.txt" ]; then
 	rm git-log-`date -I`.txt
